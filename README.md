17db023f0ebffc27b5afef6f7124a1a455177b8e
UHAKEJO <jonathan.haker@pearson.com>
2014-01-17 13:36:37 -0600

    added event listeners and updated readme

--- README.md
+++ README.md
@@ -1,91 +1,50 @@
-msbuild.js
-=======
+# msbuild - msbuild.exe for node.js
 
-about
---------
-basic msbuild operations for node.js
+basic msbuild operations (build,package,release) for node.js
 
-
-
-getting started
-----------
+## getting started
 view git project for more examples
 
 
-
-
-
-example - publish
----
+### example - build
+```
 var msbuild = require('msbuild');
+msbuild.configuration = 'myconfiguration';
+msbuild.publishProfile='mypublishProfile';
+msbuild.build();
+```
 
-msbuild.config('configuration','myconfiguration');
-
-msbuild.config('publishProfile','mypublishProfile');
-
+### example - publish w/ console logging verbose
+```
+var msbuild = require('msbuild');
+msbuild.configuration = 'myconfiguration';
+msbuild.publishProfile='mypublishProfile';
+this.overrideParams = [];
+this.overrideParams.push('/clp:ErrorsOnly');  
 msbuild.publish();
+```
 
-
-
-
-example - how to override exec to preview cmd arguments
----
+### example - how to override exec to preview cmd arguments
+```
 var msbuild = require('msbuild');
-
-msbuild.config('configuration','myconfiguration');
-
-msbuild.config('publishProfile','mypublishProfile');
-
+msbuild.configuration = 'myconfiguration';
+msbuild.publishProfile='mypublishProfile';
 // override the exec function to output cmd 
-
 msbuild.exec  = function(cmd){
-	
 	console.log('\nTEST 1: Preview MSBUILD Command');
 	console.log('********** test - start ************');
 	console.log(cmd);
 	console.log('********** test - end  ************\n');
 }
-
 msbuild.publish();
-
-
-
+```
 
 additional configuration parameters
----
-os
-
-	currently only support windows
-	
-processor
-
-	'x86', 'x64'
-	
-version
-
-	tools version; determines local path to msbuild.exe
-	
-sourcePath
-
-	'c:/mypath/mysolution.sln'   or   'c:/mypath/myproject.csproj
-	
-configuration
-
-	solution configurations; targets an environment (debug,release)  
-	
-publishProfile
-
-	publish profile; targets a specific machine (app01,app02)
-	
-outputPath
-
-	package deploy path
-	
-overrideParams
-
-	property overrides ['/p:WarningLevel=2','/p:OutputDir=bin\Debug']   
-	
-	target overrides  ['/tv:4.0']
-
-
-	
+- `os` currently only support windows
+- `processor` 	'x86', 'x64'
+- `version`	tools version; determines local path to msbuild.exe
+- `sourcePath`  'c:/mypath/mysolution.sln'   or   'c:/mypath/myproject.csproj
+- `configuration` 	solution configurations; targets an environment (debug,release)  
+- `publishProfile`  publish profile; targets a specific machine (app01,app02)
+- `outputPath`  package deploy path
+- `overrideParams`  property overrides ['/p:WarningLevel=2','/p:OutputDir=bin\Debug','/tv:4.0']  
--- msbuild.js
+++ msbuild.js
@@ -16,10 +16,13 @@ THE SOFTWARE.
 
 */
 
-//var colors = require('colors');
-
-
-
+var events = require('events'),
+	async = require('async'),
+	colors = require('colors');
+	
+var args = [];
+for(var arg in process.argv) { args.push(process.argv[arg]); }
+var help = args.splice(2,1);
 var x = function(){};
 var _ = new x();
 _.isPlainObject = function(obj){
@@ -37,6 +40,7 @@ _.isPlainObject = function(obj){
 
 var validateCmdParameter = function(param){
 	param += "";
+	param = param.trim();
 	if (param.length <= 2)  return false;
 	if (param.substring(0, 1) !== "/")  return false;
 	if (param.indexOf(":")  < 1)  return false;
@@ -53,20 +57,16 @@ var defaultValues = function(){
 		this.configuration 					= 'myconfiguration';   // solution configurations; targets an environment (debug,release)  
 		this.publishProfile 					= 'mypublishprofile';   //publish profiles; targets a specific machine (app01,app02)
 		this.outputPath 						= '';  //  'c:/deploys/release'
-		this.overrideParams		 		= [];  /***
+		this.overrideParams		 		= ['/clp:ErrorsOnly;'];  /***
 																		property overrides (example: ['/p:WarningLevel=2','/p:OutputDir=bin\Debug']  ) 
 																		target framework overrides (example:  ['/tv:4.0'] )
 																***/
 }
 
-	
 var msbuild = function(){
-
-	this.processors = {
-			'x86': 'Framework',
-			'x64': 'Framework64'
-		};
-		
+	events.EventEmitter.call(this);
+	this.showHelp = help == '?';
+	this.processors = { 	'x86': 'Framework', 'x64': 'Framework64'	};
 	this.toolsVersion = {
 		'2.0': '2.0.50727',  // can only target 2.0
 		'3.0':'3.0',
@@ -74,10 +74,11 @@ var msbuild = function(){
 		'4.0': '4.0.30319', // can target 2.0, 3.0, 3.5 and 4
 		'4.5': '4.0.30319'
 	};
-	
 };
 
 msbuild.prototype = new defaultValues();
+
+msbuild.prototype.__proto__ = events.EventEmitter.prototype;
 
 msbuild.prototype.getMSBuildPath = function(os,processor,framework){
 	if(os === 'linux') return;
@@ -101,27 +102,17 @@ msbuild.prototype.config =  function(name, value) {
 	}
 	
 	var map;
-	if (_.isPlainObject(name)) {
-		map = name;
-	} 
-	else if (value !== undefined) {
-		this[name] = value;
-		return this;
-	} else if (name === undefined) {
-		return this.values;
-	} else {
-		return this[name];
-	}
+	
+	if (_.isPlainObject(name)) { map = name; } 
+	else if (value !== undefined) { this[name] = value; return this;	} 
+	else if (name === undefined) { return this.values; } 
+	else { return this[name]; }
 
-	for (var key in map) {
-			this.values[name] = map[key];
-	}
-
+	for (var key in map) { this.values[name] = map[key]; }
 	return this;
 };
 
 msbuild.prototype.setConfig = function(cg){
-
 		this.os = 								cg.os 										|| this.os;
 		this.processor =					cg.processor 						|| this.processor;
 		this.version =						  	cg.version 								|| this.version;
@@ -130,25 +121,26 @@ msbuild.prototype.setConfig = function(cg){
 		this.publishProfile =			  	cg.publishProfile 					|| this.publishProfile;
 		this.overrideParams = 	  		cg.overrideParams 				|| this.overrideParams;
 		this.outputPath  =  				cg.outputPath 						|| this.outputPath;
-	
 }
 
 msbuild.prototype.exec = function (cmd) {
-        var childProcess = require('child_process'),
-            ls;
-
-        ls = childProcess.exec(cmd, function (error, stdout, stderr) {
-            if (error) {
-                //console.log(error.stack.redBG);
-				console.log(error.stack);
-				var errorMessage = 'Error code: ' + error.code;
-				//console.log(errorMessage.red);
-				console.log(errorMessage);
-            }
-            //console.log('RESULT: ' + stdout.grey);
-			console.log('RESULT: ' + stdout);
-        });
-    }
+	var self = this;
+	if(self.showHelp) { self.printHelp(); return; } //abort 
+	
+	var childProcess = require('child_process'), ls;
+	ls = childProcess.exec(cmd, function (error, stdout, stderr) {
+		var msg = '';
+		if (error) {
+			msg = ('\nstack...\n' + error.stack).grey+('\n publish failed - errors'.white.redBG);
+			self.emit('error',error.code,msg);
+			return;
+		}
+		else{
+			msg = stdout.grey+('\n publish finished - no errors'.white.greenBG);
+			self.emit('done',null,msg);
+		}
+	});
+}
 	
 msbuild.prototype.getDeployOnBuildParam = function(shouldDeploy){
 		if(!shouldDeploy){
@@ -169,7 +161,6 @@ msbuild.prototype.getBuildParams = function(params){
 		if(params.indexOf('publishprofile') === -1 && this.publishProfile){
 			params += (' /p:publishprofile=' + this.publishProfile + ' ');
 		}
-		
 		return params;
 }
 
@@ -220,38 +211,38 @@ msbuild.prototype.getOverrideParams = function(params){
 }
 	
 msbuild.prototype.build = function(){
+	this.emit('status',null,'build starting');
 	var params = this.getDeployOnBuildParam(false);
 		 params = this.getBuildParams(params);
-	
 	var buildpath = this.buildexe();
 	var cmd = buildpath.concat(' ',this.sourcePath,' ',params);
 	this.exec(cmd);
 }
 
 msbuild.prototype.package = function(){
+	this.emit('status',null,'package starting');
 	var params = this.getBuildParams();
 		 params = this.getOverrideParams(params);
 		 params = this.getPackageParams(params);
-	
 	var cmd = this.buildexe().concat(' ',this.sourcePath,' ',params);
 	this.exec(cmd);
 }
 
 msbuild.prototype.publish = function(){
+	this.emit('status',null,'publish starting');
 	var params = this.getBuildParams();
 		 params = this.getOverrideParams(params);
 		 params = this.getPublishParams(params);
-	
 	var buildpath = this.buildexe();
 	var cmd = buildpath.concat(' ',this.sourcePath,' ',params);
 	this.exec(cmd);
+	
 }
 
-
-
 /****  help section ****/
-function printHelp(o){
-	var helpFunctionsToIgnore = ['exec','path','buildexe'];
+msbuild.prototype.printHelp = function(){
+	var o = this;
+	var helpFunctionsToIgnore = ['exec','path','buildexe','on','once','emit','addListener','removeListener','removeAllListeners','listeners','setMaxListeners','printHelp'];
 	console.log("\nfunctions".cyan.bold);
 	console.log('*******************'.cyan);
 	
@@ -263,12 +254,17 @@ function printHelp(o){
 			console.log(p.redBG);
 		}
 	}
+	
+	console.log("\nevents".cyan.bold + ' [err,results]'.grey);
+	console.log('*******************'.cyan);
+	console.log('status'.redBG+' // msbuild.on(\'status\',myfunc)'.grey);
+	console.log('error'.redBG+' // msbuild.on(\'error\',myfunc)'.grey);
+	console.log('done'.redBG+' // msbuild.on(\'done\',myfunc)'.grey);
 }
 
-var args = [];
-for(var arg in process.argv) { args.push(process.argv[arg]); }
-var help = args.splice(2,1);
-if(help == '?') { 	printHelp( new msbuild()); } 
 
-
-module.exports = new msbuild();
+var msb = new msbuild();
+msb.on('status',function(err,results){ if(this.showHelp) return; if(err){ console.log(err.redBG);}; console.log(results.cyan);});
+msb.on('error',function(err,results){ console.log('error'.red); if(err){ console.log(err.redBG);}; console.log(results.red);});
+msb.on('done',function(err,results){console.log(results);});
+module.exports = msb;
